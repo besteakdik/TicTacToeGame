b705893e8c12a8c4d2bf1efd3bbe13510b28a820
U180709002 <YOUR>
2021-11-05 23:07:11 +0300

    tictactoe

---
+++ app/src/main/java/com/example/tictactoegame/MainActivity.java
@@ -0,0 +1,141 @@
+package com.example.tictactoegame;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    static final String PLAYER_1 = "X";
+    static final String PLAYER_2 = "O";
+
+    boolean player1Turn = true;
+    byte[][] board = new byte[3][3];
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TableLayout tableLayout = findViewById(R.id.board);
+        for (int i = 0; i < 3; i++) {
+            TableRow row = (TableRow) tableLayout.getChildAt(i);
+            for (int j = 0; j < 3; j++) {
+                Button btn = (Button) row.getChildAt(j);
+                btn.setOnClickListener(new CellListener(i, j));
+            }
+        }
+        /*Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(new CellListener());*/
+    }
+
+
+    class CellListener implements View.OnClickListener {
+        int row, col;
+
+        public CellListener(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (!isCellOccupied(row, col)) {
+                Toast.makeText(MainActivity.this, "Cell is already occupied", Toast.LENGTH_LONG).show();
+                return;
+            }
+            if (player1Turn) {
+                ((Button) view).setText(PLAYER_1);
+                board[row][col] = 1;
+            } else {
+                ((Button) view).setText(PLAYER_2);
+                board[row][col] = 2;
+            }
+
+            if (gameEnded(row,col) == -1) {
+                player1Turn = !player1Turn;
+            } else if (gameEnded(row,col) == 0) {
+                Toast.makeText(MainActivity.this, "It is a DRAW", Toast.LENGTH_LONG).show();
+            } else if (gameEnded(row,col) == 1){
+                Toast.makeText(MainActivity.this, "Player 1 WINS", Toast.LENGTH_LONG).show();
+            }else{
+                Toast.makeText(MainActivity.this, "Player 2 WINS", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private boolean isCellOccupied(int row, int col) {
+        return (board[row][col] == 0);
+    }
+
+    private int gameEnded(int row, int col) {
+        int player = board[row][col];
+        boolean win = true;
+        for (int i=0; i < 3; i++) {
+            if ((board[i][col]) != player)
+                win = false;
+                break;
+        }
+        if (win){
+            return player;
+    }
+    return -1;
+}
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putBoolean("Player1Turn", player1Turn);
+        //.putInt("moveCount", moveCount);
+        byte[] boardOneDimension = new byte[9];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                boardOneDimension[3 * i + j] = board[i][j];
+            }
+        }
+        outState.putByteArray("board", boardOneDimension);
+        super.onSaveInstanceState(outState);
+    }
+
+        /*win = true;
+        for (int i = 0; i < 3; i++) {
+            if ((board[row][i]) != player)
+                win = false;
+            break;
+
+            if (moveCount == 9)
+                return 0;
+            return -1;
+
+            boolean emptyCell = false;
+            for (int i = 0; i < 3; i++) {
+
+            }*/
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        player1Turn = savedInstanceState.getBoolean("player1Turn");
+        byte[] boardSingle = savedInstanceState.getByteArray("board");
+        for (int i=0; i<9; i++) {
+            board[i / 3][i%3] = boardSingle[i];
+        }
+        TableLayout tableLayout = findViewById(R.id.board);
+        for (int i = 0; i < 3; i++) {
+            TableRow row = (TableRow) tableLayout.getChildAt(i);
+            for (int j = 0; j < 3; j++) {
+                Button btn = (Button) row.getChildAt(j);
+                if (board[i][j] == 1){
+                    btn.setText("X");
+                }else if (board[i][j] == 2) {
+                    btn.setText("O");
+                }
+
+            }
+        }
+    }
+}
